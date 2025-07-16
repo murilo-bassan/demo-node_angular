5dc482719bb01dce0d42facbe1b1694b9a95af01
murilo_bassan <murilogabassan@gmail.com>
2025-07-16 10:57:47 -0400

    feat: rotas

--- spa-frontend/src/app/app.routes.ts
+++ spa-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { App } from './app';
+import {Other} from './components/other/other'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home
+    },
+    {
+        path: "home",
+        component: Home
+    },
+    {
+        path: "other",
+        component: Other
+    }
+];
